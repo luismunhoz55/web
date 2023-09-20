f5ac3e3a54d1b73f2c9b9c5557f3f931603aaba0
luismunhoz55 <munhozluisfernando@gmail.com>
2023-09-20 13:35:12 -0300

    first page

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Tabs, TabsList, TabsTrigger, TabsContent } from './components/ui/tabs'
+import { Label } from './components/ui/label'
+import { Input } from './components/ui/input'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+    <main className='w-full h-screen flex items-center justify-center'>
+      <Tabs defaultValue="account" className="w-[400px] p-4 rounded-[6px] bg-muted flex flex-col items-center">
+        <TabsList>
+          <TabsTrigger className='rounded-l' value="account">Account</TabsTrigger>
+          <TabsTrigger className='rounded-r' value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent className='flex items-start flex-col bg-background w-full h-full p-4 rounded-[6px] gap-4' value="account">
+
+          <div className='flex flex-col gap-2'>
+            <h1 className='text-2xl font-bold'>Account</h1>
+            <p className='text-slate-400 poppins text-sm'>Make changes to your account here. Click save when you're done.</p>
+          </div>
+
+          <div className='flex flex-col gap-2 w-full'>
+            <Label htmlFor='name' className='text-sm'>Name</Label>
+            <input type="text" className='w-full p-2 bg-background rounded-[6px] border-solid border-secondary border' placeholder='Luis Munhoz' name="name" id="name" />
+          </div>
+
+          <div className='flex flex-col gap-2 w-full'>
+            <Label htmlFor='username' className='text-sm'>Username</Label>
+            <input type="text" className='w-full p-2 bg-background rounded-[6px] border-solid border-secondary border' placeholder='@luuis.munhoz' name="username" id="username" />
+          </div>
+
+          <Button className='bg-primary rounded-[6px] hover:bg-primary/70' variant={'secondary'}>Save changes</Button>
+
+        </TabsContent>
+        <TabsContent value="password">
+
+        </TabsContent>
+      </Tabs>
+    </main>
+
   )
 }
 
